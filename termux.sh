12168ed978546a8e46c185e022a9202638116d34
tcxone <tcxone@outlook.com>
2023-07-28 18:00:01 +0800

    Create termux.sh

---
+++ termux.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+printf "========== start =========="
+termux-setup-storage
+termux-change-repo
+
+printf "\n\n========== install some packages [total 11] =========="
+pkg install git zsh vim wget python golang openjdk-17 alist termux-api tur-repo code-server
+printf "\n\n========== successfully install packages =========="
+
+printf "\n\n========== install ohmyzsh =========="
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+printf "\n\n========== successfully install ohmyzsh =========="
+
+printf "\n\n========================"
+  printf "\n=      @      @@@@@@   ="
+  printf "\n=     @ @         @    ="
+  printf "\n=    @   @       @     ="
+  printf "\n=   @ @@@ @     @      ="
+  printf "\n=  @       @   @@@@@@  ="
+  printf "\n===================done="
