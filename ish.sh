6b52a4164d24271b789e399939d7c113a83bbb31
tcxone <tcxone@outlook.com>
2023-07-28 16:03:32 +0800

    Create ish.sh

---
+++ ish.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "----------start----------"
+
+sed -i '$a alias "la"="ls -al"' /etc/profile
+echo "Tip: la == ls-al"
+
+read -n 1 -p "Will install Python3 vim git zsh bash [Y/n] >>>" yn1
+if(($yn1=="Y"))
+then
+	apk add Python3 vim git zsh bash
+	echo "succsee"
+else
+	echo "Do NOT install"
+fi
+
+read -n 1 -p "Will install ohmyzsh [Y/n] >>>" yn2
+if(($yn2=="Y"))
+then
+	sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+	echo "success"
+else
+	echo "Do NOT install"
+fi
+
+echo "----------done----------"
