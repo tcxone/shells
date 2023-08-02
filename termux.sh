a4b9d83e1f9283ba54ee3688ac7b6fd9df23b926
tcxone <tcxone@outlook.com>
2023-08-02 23:44:10 +0800

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -6,18 +6,10 @@ termux-setup-storage
 
 printf "\n\n========== install some packages [total 11] =========="
 pkg upgrade
-pkg install git zsh vim wget python golang openjdk-17 alist
-pkg install termux-api tur-repo code-server
+pkg install git zsh vim wget python golang openjdk-17 alist termux-api tur-repo openssh
+pkg install code-server
 printf "\n\n========== successfully install packages =========="
 
 printf "\n\n========== install ohmyzsh =========="
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 printf "\n\n========== successfully install ohmyzsh =========="
-
-printf "\n\n========================"
-  printf "\n=      @      @@@@@@   ="
-  printf "\n=     @ @         @    ="
-  printf "\n=    @   @       @     ="
-  printf "\n=   @ @@@ @     @      ="
-  printf "\n=  @       @   @@@@@@  ="
-  printf "\n===================done="
